c4ea78ab3780fdb422c017e70536be3e3d1a6b82
Ab-mahin <mahin.cse7.bu@gmail.com>
2023-09-20 01:38:37 +0600

    Create LOJ-1336.cpp

---
+++ LOJ-1336.cpp
@@ -0,0 +1,69 @@
+// USER: "Ab.Mahin"
+// Problem: Sigma Function
+// Contest: LightOJ
+// URL: https://lightoj.com/problem/sigma-function
+// Memory Limit: 64 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long int
+//#define m 1000000007
+
+
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+//Findout buggs:
+
+#define BUG
+
+#ifdef BUG
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+cout << name << " : " << arg1 << '\n';
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+const char* comma = strchr(names + 1, ',');
+cout.write(names, comma - names) << " : " << arg1<< " | ";__f(comma+1, args...);
+}
+#else
+#define bug(...)
+#endif
+
+//...................
+const ll N = 1e6 + 5;
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    
+    /*#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif*/
+
+    ll t, tt;
+    t = tt = 1;
+    
+    cin >> t;
+	
+    while(t--){
+        ll n;
+        cin >> n;
+        
+        cout << "Case " << tt++ << ": " << n - ((ll)sqrt(n) + (ll)sqrt(n / 2)) << '\n';
+        
+    }
+    
+    
+    return 0;
+}
