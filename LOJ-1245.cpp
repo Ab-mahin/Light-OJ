4e10bc246b62dc2c17da7a2c596232f30ecd7659
Ab-mahin <mahin.cse7.bu@gmail.com>
2023-09-20 01:38:42 +0600

    Create LOJ-1245.cpp

---
+++ LOJ-1245.cpp
@@ -0,0 +1,77 @@
+// USER: "Ab.Mahin"
+// Problem: Harmonic Number (II)
+// Contest: LightOJ
+// URL: https://lightoj.com/problem/harmonic-number-ii
+// Memory Limit: 64 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long int
+//#define m 1000000007
+
+
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+//Findout buggs:
+
+#define BUG
+
+#ifdef BUG
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+cout << name << " : " << arg1 << '\n';
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+const char* comma = strchr(names + 1, ',');
+cout.write(names, comma - names) << " : " << arg1<< " | ";__f(comma+1, args...);
+}
+#else
+#define bug(...)
+#endif
+
+//...................
+const ll N = 2e5 + 5;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    
+    /*#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif*/
+
+    ll t, tt;
+    t = tt = 1;
+    
+    cin >> t;
+	
+    while(t--){
+        ll n;
+        cin >> n;
+        
+        ll x = n, y = 0, ans = 0;
+        for(ll i = 1; i <= round(sqrt(n + 1)); i++){
+        	if((n / i) != (i - 1)) ans += (n / i);
+        	x = (n / i);
+        	ans += (y - x) * (i - 1);
+        	y = (n / i);
+        }
+        cout << "Case " << tt++ << ": " << ans << '\n';
+        
+    }
+    
+    
+    return 0;
+}
