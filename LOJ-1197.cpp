0efbd487dc5f1e658c871be83bf8e08c113ae81d
Ab-mahin <mahin.cse7.bu@gmail.com>
2023-09-20 01:38:55 +0600

    Create LOJ-1197.cpp

---
+++ LOJ-1197.cpp
@@ -0,0 +1,98 @@
+// USER: "Ab.Mahin"
+// Problem: Help Hanzo
+// Contest: LightOJ
+// URL: https://lightoj.com/problem/help-hanzo
+// Memory Limit: 64 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long int
+//#define m 1000000007
+
+
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+//Findout buggs:
+
+#define BUG
+
+#ifdef BUG
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+cout << name << " : " << arg1 << '\n';
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+const char* comma = strchr(names + 1, ',');
+cout.write(names, comma - names) << " : " << arg1<< " | ";__f(comma+1, args...);
+}
+#else
+#define bug(...)
+#endif
+
+//...................
+const ll N = 2e5 + 5;
+
+vector<bool> segmentedSieve(ll L, ll R) {
+    ll lim = sqrt(R);
+    vector<bool> mark(lim + 1, false);
+    vector<ll> primes;
+    for (ll i = 2; i <= lim; ++i) {
+        if (!mark[i]) {
+            primes.emplace_back(i);
+            for (ll j = i * i; j <= lim; j += i)
+                mark[j] = true;
+        }
+    }
+
+    vector<bool> isPrime(R - L + 1, true);
+    for (ll i : primes)
+        for (ll j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    
+    /*#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif*/
+
+    ll t, tt;
+    t = tt = 1;
+    
+    cin >> t;
+	
+    while(t--){
+        ll l, r;
+        cin >> l >> r;
+        
+        vector<bool> ans = segmentedSieve(l ,r);
+        
+        ll res = 0;
+        for(auto i : ans){
+        	res += i;
+        }
+        
+        cout << "Case " << tt++ << ": " << res << '\n';
+        
+    }
+    
+    
+    return 0;
+}
