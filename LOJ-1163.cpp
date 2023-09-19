5cd835b12c6f0f7e521f60568214777cdf7b14d5
Ab-mahin <mahin.cse7.bu@gmail.com>
2023-09-20 01:38:29 +0600

    Create LOJ-1163.cpp

---
+++ LOJ-1163.cpp
@@ -0,0 +1,77 @@
+// USER: "Ab.Mahin"
+// Problem: Bank Robbery
+// Contest: LightOJ
+// URL: https://lightoj.com/problem/bank-robbery
+// Memory Limit: 64 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll unsigned long long
+//#define m 1000000007
+
+
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+//Findout buggs:
+
+#define BUG
+
+#ifdef BUG
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+cout << name << " : " << arg1 << '\n';
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+const char* comma = strchr(names + 1, ',');
+cout.write(names, comma - names) << " : " << arg1<< " | ";__f(comma+1, args...);
+}
+#else
+#define bug(...)
+#endif
+
+//...................
+const ll N = 2e5 + 5;
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    
+    /*#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif*/
+
+    ll t, tt;
+    t = tt = 1;
+    
+    cin >> t;
+	
+    while(t--){
+        ll n;
+        cin >> n;
+        
+       	ll ans = (n * 10) / 9;
+       	
+       	cout << "Case " << tt++ << ": ";
+       	
+       	if(n % 9){
+       		cout << ans << '\n';
+       	}
+       	else cout << ans - 1 << ' ' << ans << '\n';
+       	
+    }
+    
+    
+    return 0;
+}
