4e916d21c977542c90c787d250b8e0987ddfcbc9
Ab-mahin <mahin.cse7.bu@gmail.com>
2023-09-20 01:39:00 +0600

    Create LOJ-1045.cpp

---
+++ LOJ-1045.cpp
@@ -0,0 +1,72 @@
+// USER: "Ab.Mahin"
+// Problem: Digits of Factorial
+// Contest: LightOJ
+// URL: https://lightoj.com/problem/digits-of-factorial
+// Memory Limit: 64 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long int
+//#define m 1000000007
+
+
+template <typename T>
+using ordered_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+//Findout buggs:
+
+#define BUG
+
+#ifdef BUG
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+cout << name << " : " << arg1 << '\n';
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+const char* comma = strchr(names + 1, ',');
+cout.write(names, comma - names) << " : " << arg1<< " | ";__f(comma+1, args...);
+}
+#else
+#define bug(...)
+#endif
+
+//...................
+const ll N = 1e6 + 5;
+double fac[N];
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    
+    /*#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif*/
+
+    ll t, tt;
+    t = tt = 1;
+    
+    cin >> t;
+    
+    for(ll i = 1; i < N; i++){
+    	fac[i] = fac[i - 1] + log(i);
+    }
+	
+    while(t--){
+        ll n, b;
+        cin >> n >> b;
+        cout << "Case " << tt++ << ": " << (ll)((fac[n]) / log(b)) + 1 << '\n';
+    }
+    
+    
+    return 0;
+}
